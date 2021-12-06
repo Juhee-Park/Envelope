ea311e49757959d44b20bb5410d351ae7f6fb140
You-jeong136 <59916029+You-jeong136@users.noreply.github.com>
2021-12-06 20:18:39 +0900

    Update clickEvent.cs

--- clickEvent.cs
+++ clickEvent.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class clickEvent : MonoBehaviour
 { 
     private RaycastHit hit;
-    bool isPickUp; //열쇠 얻었는지에 대한 상태
+    private bool isPickUp = false; //열쇠 얻었는지에 대한 상태
 
     // Update is called once per frame
     void Update()
     {
+        // 기본 상호작용 용 _ 마우스를 누를 경우. _ 추가 창 open.
+        if (Input.GetMouseButtonDown(0)) {
+        
+        }
 
-        //물체 클릭 할 경우. 
-        if (Input.GetMouseButtonDown(0))
+        //물체 클릭 할 경우. (완전히) 
+        if (Input.GetMouseButtonUp(0))
         {
 
             //마우스 포지션 취득해서 대입
@@ -27,33 +31,45 @@ public class clickEvent : MonoBehaviour
 
                 //상호작용할 오브젝트_ 이름으로 인식 (열쇠는 Key라 가정.)
                 if (hit.transform.gameObject.name == "Key")
-                {   
+                {
                     Debug.Log("click Key");
-                    
-                    //먹은 상태로 변경.
+
+                    //먹은 상태로 변경. 
                     isPickUp = true;
-                    
-                    //상호작용할 코드 주입.
 
 
-
-                    //먹은 아이템 화면 상 삭제.
-                    //Destroy(hit.transform.gameObject)
+                    //먹은 아이템 화면 상 삭제. 
+                    Destroy(hit.transform.gameObject);
 
                 }
                 //버튼을 누를 경우 ~ 상호작용. 오브젝트 이름에 button이 있을 경우.
                 else if (hit.transform.gameObject.name.Contains("button"))
                 {
-                    //상호작용할 코드 주입.  
                     Debug.Log("click button");
 
+                    //아래에 버튼을 클릭할 경우 _ 버튼이 눌리는 작용 외 추가 상호작용할 내용 추가
+
                 }
+                //기타 상호작용.
                 else
                 {
-                    //그 외 클릭 이벤트. 
+                    //열쇠 얻은 후, 타 오브젝트 클릭 시 열쇠 상태가 유지 되는지 확인용
+                    /* if (isPickUp)
+                     {
+                         Debug.Log("pick up true");
+                     }
+                     else {
+                         Debug.Log("pick up false");
+                     }*/
+
                 }
 
             }
         }
+  
+    }
+
+    public bool getIsPickUp() {
+        return isPickUp;
     }
 }
